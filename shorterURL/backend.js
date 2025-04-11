7fb6370fa2d2e600ea78acb8a529fc6f6f2b6ca1
CoolShivas <shivas2710cool00@gmail.com>
2025-04-11 13:17:45 +0530

    Creation of Backend file.

---
+++ shorterURL/backend.js
@@ -0,0 +1,10 @@
+console.log("Formation of Backend File.");
+console.log("To render the server i.e., port localhost as well as CRUD operation.");
+console.log("CRUD means the requests for the Create, Read, Undo, Delete.");
+console.log("i.e., GET, POST,Patch, EDIT, DELETE");
+// // // // Here, we are getting the Output on Terminal as :-
+// // // // Formation of Backend File.
+// // // // To render the server i.e., port localhost as well as CRUD operation.
+// // // // CRUD means the requests for the Create, Read, Undo, Delete.
+// // // // i.e., GET, POST,Patch, EDIT, DELETE
+// // // // Completed running 'backend.js'
