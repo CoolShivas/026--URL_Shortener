2716f6644990b2bbc6a0d03ae8688de91ca259b3
CoolShivas <shivas2710cool00@gmail.com>
2025-04-11 16:53:31 +0530

    Moved the style.css file in the Public folder. Addition of one more url for the styling the page.

--- shorterURL/backend.js
+++ shorterURL/backend.js
@@ -41,6 +41,25 @@ const PORT = 5000;
                      // // Response shows the text on the browser page i.e, 404 Page not found;
                  }
              }
+             else if(request.url === "/style.css")
+             {
+                try {
+                    const handlingData = await readFile(path.join("public", "style.css"));
+                    // // Use of readFile to read the files content;
+                    // // As well as path used to locate the folder and file;
+                    response.writeHead(200, {"Content-Type" : "text/css"});
+                    // // If request is successfull then access the file content;
+                    response.end(handlingData);
+                    // // And, then data is visible on the browser screen of this index.html file;
+                } catch (error) {
+                    // // If there were any error;
+                    response.writeHead(404, {"Content-Type" : "text/css"});
+                    // // Response created the error page i.e, 404 with content type 
+                    // // i.e., css file;
+                    response.end("404 Page not found");
+                    // // Response shows the text on the browser page style i.e, 404 Page not found;
+                }
+             }
          }
  });
 
